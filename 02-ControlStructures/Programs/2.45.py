36129ce0bc40a45a74f14aec2627f0966d1dcfee
OskarKozaczka <oskaro0@vp.pl>
2019-10-27 22:24:35 +0100

    !

--- 02-ControlStructures/Programs/2.45.py
+++ 02-ControlStructures/Programs/2.45.py
@@ -1,11 +1,21 @@
-from math import sqrt
-n = int(input("Wprowadz liczbe n: "))
-liczby = [2,3]
+x=int(input("Podaj Liczbę: "))
 
-kandydat=int(liczby[2:])+1
-for i in range(10):
-    for j in range(len(liczby)):
-        while(liczby[j]<=sqrt(kandydat)):
-            if(kandydat%liczby[i]==0):
-                liczby.append(kandydat)
-        
+
+c=0
+c2=0
+
+threshold=2
+    
+    
+while c2<x:    
+    
+    for i in range(1,threshold):
+        if threshold%i==0:
+            c+=1
+
+    if c==1:
+        print (threshold)
+        c2+=1
+    threshold+=1
+    c=0
+
