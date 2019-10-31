5179ee9a7d79fa04a99218f54234385a7d228f33
OskarKozaczka <oskaro0@vp.pl>
2019-11-01 00:27:36 +0100

    .

--- 03-FileHandling/Programs/3.24.py
+++ 03-FileHandling/Programs/3.24.py
@@ -3,12 +3,14 @@ i=0
 tab=[['Marek','Zelnik','zelnik@sed.pl'],['Ewa','Maj','maje@wp.pl'],['Piotr','Wyga','wygap@gop.pl']]
 
 
-with open('3.24.txt', 'w') as file:
+with open('3.24.csv', 'w') as file:
     for c in range(0,len(tab)):
         if c>0:
             file.write("\n")
         for i in range(0, len(tab)):
-            file.write(tab[c][i]+" ")
+            file.write(tab[c][i])
+            if i!=len(tab)-1:
+                file.write(",")
     
 
                        
